e31f612eb84a40f5cf40d32ae9c4d0abf4652fa1
Aymene Abdelmoumene Boukhelkhal <aymenxyz6@gmail.com>
2021-11-16 21:44:26 +0100

    fixing ui

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -1,4 +1,4 @@
-var nodemailer = require('nodemailer');
+var nodemailer = require('nodema');
 var smtpTransport = require('nodemailer-smtp-transport');
 const config = require('./config');
 
@@ -11,16 +11,24 @@ var transporter = nodemailer.createTransport(smtpTransport({
   }
 }));
 
-module.exports.sendEmail() = (emailToSend) => {
+module.exports.sendEmail = (emailToSend,fileName) => {
+    const text = 'Hi there, \n \n Please see attached the order report. \n \n Regards'
     var mailOptions = {
         from: config.config.email.email,
         to: emailToSend,
         subject: 'New Filtred Data',
-        text: 'That was easy!'
+        text: text,
+        attachments: [
+            {
+                fileName:fileName,
+                path:`./routes/${fileName}`
+            }
+        ]
       };
       return new Promise(async (resolve) => {
         transporter.sendMail(mailOptions, function(error, info){
             if (error) {
+              console.log(error);
               resolve(false);
             } else {
               resolve(true);
--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,13 @@ const fakeUa = require('fake-useragent');
 const cors = require('./cors');
 /* GET users listing. */
 
-const writeXlsxFile = require('write-excel-file');
-
+ const nodemailer = require('./nodemailer');
 router.all('/')
   .options('/', cors.corsWithOptions, function (req, res, next) {
     next();
   })
   .post('/', cors.corsWithOptions, async function (req, response, next) {
+    try {
     const fileNames = req.body.email.split('@')[0] + new Date().toISOString() +  '.xlsx';
     const headingColumnNames = req.body.columns;
     const rows = remoduleRows(req.body.rows, headingColumnNames);
@@ -35,9 +35,24 @@ router.all('/')
       });
       rowIndex++;
     });
-    wb.write(`${fileNames}`);
-    response.statusCode = 200;
-    response.json({ success: true, msg: 'Sent' });
+    wb.write(`./routes/${fileNames}`);
+    nodemailer.sendEmail(req.body.email,fileNames)
+      .then((result) => {
+        if (result && result == true){
+          response.statusCode = 200;
+          response.json({ success: true, msg: 'Email Sent Successfully' });
+        }
+        else {
+          response.statusCode = 500;
+          response.json({ success: false, msg: 'File Created But error while sending' });
+        }
+      })
+   
+  }
+  catch(er) {
+    response.statusCode = 500;
+    response.json({success:false,msg:'Something Went Wrong !'});
+  }
   });
 
 function remoduleRows(rows, columns) {
@@ -45,7 +60,6 @@ function remoduleRows(rows, columns) {
   rows.map(row => {
     const newRow = {};
     row.map((rowData, index) => {
-      console.log(columns[index]);
       const key = columns[index]
       newRow[columns[index]] = rowData.toString();
     })
@@ -54,61 +68,5 @@ function remoduleRows(rows, columns) {
   return newRows;
 }
 
-function test() {
-  [{
-    firstName: 'John',
-    lastName: 'Bailey',
-    purchasePrice: 1000,
-    paymentsMade: 100
-  }, {
-    firstName: 'Leonard',
-    lastName: 'Clark',
-    purchasePrice: 1000,
-    paymentsMade: 150
-  }, {
-    firstName: 'Phil',
-    lastName: 'Knox',
-    purchasePrice: 1000,
-    paymentsMade: 200
-  }, {
-    firstName: 'Sonia',
-    lastName: 'Glover',
-    purchasePrice: 1000,
-    paymentsMade: 250
-  }, {
-    firstName: 'Adam',
-    lastName: 'Mackay',
-    purchasePrice: 1000,
-    paymentsMade: 350
-  }, {
-    firstName: 'Lisa',
-    lastName: 'Ogden',
-    purchasePrice: 1000,
-    paymentsMade: 400
-  }, {
-    firstName: 'Elizabeth',
-    lastName: 'Murray',
-    purchasePrice: 1000,
-    paymentsMade: 500
-  }, {
-    firstName: 'Caroline',
-    lastName: 'Jackson',
-    purchasePrice: 1000,
-    paymentsMade: 350
-  }, {
-    firstName: 'Kylie',
-    lastName: 'James',
-    purchasePrice: 1000,
-    paymentsMade: 900
-  }, {
-    firstName: 'Harry',
-    lastName: 'Peake',
-    purchasePrice: 1000,
-    paymentsMade: 1000
-  }]
 
-
-}
-
-test();
 module.exports = router;
