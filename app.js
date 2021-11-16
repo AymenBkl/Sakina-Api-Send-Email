31e6bcab03668ae20d6b7dbb00efeda7d0b82891
Aymene Abdelmoumene Boukhelkhal <aymenxyz6@gmail.com>
2021-11-16 23:25:47 +0100

    working

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var scrapRouter = require('./routes/users');
+var mainRouter = require('./routes/main');
 
 var app = express();
 
@@ -19,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/email', scrapRouter);
-
+app.use('/',mainRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ routes/main.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+const request = require('request');
+const fakeUa = require('fake-useragent');
+
+const cors = require('./cors');
+/* GET users listing. */
+
+ const nodemailer = require('./nodemailer');
+router.all('/')
+  .options('/', cors.corsWithOptions, function (req, res, next) {
+    next();
+  })
+  .get('/', cors.corsWithOptions, async function (req, response, next) {
+    response.sendFile('./views/index.html');
+  });
+
+
+
+
+
+module.exports = router;
